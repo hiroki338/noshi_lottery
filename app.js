5da28d1ab7d8535558e0f3244ad13c7c7795df7e
hiroki338 <michaelweex@gmail.com>
2023-12-15 16:38:18 +0800

    Update website with new changes

--- app.js
+++ app.js
@@ -1,114 +1,108 @@
 const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
 const abi = [
-	{
-		"inputs": [],
-		"name": "enter",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "pickWinner",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [],
-		"name": "getLastWinner",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getParticipants",
-		"outputs": [
-			{
-				"internalType": "address payable[]",
-				"name": "",
-				"type": "address[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "lastWinner",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "manager",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "participants",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "pickWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getLastWinner",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getParticipants",
+        "outputs": [
+            {
+                "internalType": "address payable[]",
+                "name": "",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "lastWinner",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "participants",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
- 
+
 let contract;
 let web3;
 let accounts;
- 
-    async function initApp() {
-        // Check if the web3 object is available
-        if (typeof window.ethereum !== "undefined") {
-            if (typeof Web3 === "undefined") {
-                const Web3 = require("web3");
-                web3 = new Web3(window.ethereum);
-            } else {
-                web3 = new Web3(window.ethereum);
-            }   
-            accounts = await web3.eth.getAccounts();
-            contract = new web3.eth.Contract(abi, contractAddress);
-    
+
+async function initApp() {
+    // Check if the web3 object is available
+    if (typeof window.ethereum !== "undefined") {
+        if (typeof Web3 === "undefined") {
+            const Web3 = require("web3");
+            web3 = new Web3(window.ethereum);
+        } else {
+            web3 = new Web3(window.ethereum);
         }
+        accounts = await web3.eth.getAccounts();
+        contract = new web3.eth.Contract(abi, contractAddress);
+    }
 
     document.getElementById('enterLottery').addEventListener('click', () => {
         if (typeof web3 !== "undefined" && typeof accounts !== "undefined") {
@@ -122,7 +116,7 @@ let accounts;
             console.error("Web3 not available. Please install MetaMask or another Ethereum provider.");
         }
     });
- 
+
     document.getElementById('pickWinner').addEventListener('click', () => {
         contract.methods.pickWinner().send({ from: accounts[0] })
             .then(() => {
@@ -132,26 +126,27 @@ let accounts;
             })
             .catch(console.error);
     });
- 
-    // Function to display participants in the browser HTML
-    function updateParticipantsList() {
-        contract.methods.getParticipants().call()
-            .then(displayParticipants)
-            .catch(console.error);
-    }
-
-    function displayWinner() {
-        contract.methods.getLastWinner().call()
-            .then(winner => {
-                const winnerElement = document.getElementById('winner');
-                winnerElement.innerHTML = `<p>Winner: ${winner}</p>`;
-            })
-            .catch(console.error);
-    }
-
-    function displayParticipants(participants) {
-        const participantsList = document.getElementById('participantsList');
-        participantsList.innerHTML = participants.map(address => `<li>${address}</li>`).join('');
-    }
 }
+
+// Function to display participants in the browser HTML
+function updateParticipantsList() {
+    contract.methods.getParticipants().call()
+        .then(displayParticipants)
+        .catch(console.error);
+}
+
+function displayWinner() {
+    contract.methods.getLastWinner().call()
+        .then(winner => {
+            const winnerElement = document.getElementById('winner');
+            winnerElement.innerHTML = `<p>Winner: ${winner}</p>`;
+        })
+        .catch(console.error);
+}
+
+function displayParticipants(participants) {
+    const participantsList = document.getElementById('participantsList');
+    participantsList.innerHTML = participants.map(address => `<li>${address}</li>`).join('');
+}
+
 initApp();
